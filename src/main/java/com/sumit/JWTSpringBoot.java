5d75438a1f754964a4b588ca777f23efe242b6fd
Sumit Mittal <sumitmittal.adonis@gmail.com>
2023-12-12 20:15:20 -0500

    JWT in Spring Boot

--- src/main/java/com/sumit/JWTSpringBoot.java
+++ src/main/java/com/sumit/JWTSpringBoot.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWithJWT {
+public class JWTSpringBoot {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWithJWT.class, args);
+		SpringApplication.run(JWTSpringBoot.class, args);
 	}
 
 }
